cb4663bae7bedcbfa1cc8bcc3cf5885e12caba03
Hack Reactor Student <student@hackreactor.com>
2017-07-29 11:24:21 -0700

    Nest queries into callbacks for POST

--- server/models/index.js
+++ server/models/index.js
@@ -1,30 +1,43 @@
-var db = require('../db');
+var db = require('../db').connection;
 
 module.exports = {
   messages: {
     get: function (callback) {
-      db.connection.query('SELECT messages.text, users.username FROM messages, users', function(error, results, field) {
+      db.query('SELECT messages.text, users.username FROM messages, users', function(error, results, field) {
         if (error) { throw error; }
         callback(results); 
       });
     },
     post: function (body, callback) {
-      // db.connection.query(`insert into users (username) values('${body.username}')`, function(error, results, field) {
-      //   if (error) { throw error; }
-      // });
-      db.connection.query(`insert into messages (text) values('${body.text}')`, function(error, results, field) {
+      // if (error) { throw error; }
+      db.query(`insert into users (username) values('${body.username}')`, function(error, results, field) {
         if (error) { throw error; }
-        callback();
-      });
-            
-      // db.connection.query(`insert into messages 
-      //   (text, user_id, room_id) 
-      //   values('${body.text}', select id from users where username='${body.username}', select id from rooms where roomname='${body.roomname}')`, 
-      // function(error, results, field) {
-      //   if (error) { throw error; }
-      // });
+        db.query(`insert into rooms (roomname) values('${body.roomname}')`, function(error, results, field) {
+          if (error) { throw error; }
+          db.query(`SELECT users.id FROM users WHERE username='${body.username}'`, 
+          function(error, results, field) {
+            var userId = results[0].id;
+            db.query(`SELECT rooms.id FROM rooms WHERE roomname='${body.roomname}'`, 
+            function(error, results, field) {
+              var roomId = results[0].id;
+              db.query(`insert into messages (text, user_id, room_id) 
+                values (
+                  '${body.text}', 
+                  '${userId}', 
+                  '${roomId}'
+                )`, 
+              function(error, results, field) {
+                if (error) { throw error; }
+                callback();
+              });
+            });
+          });
+        });
+      });   
     }
   },
+
+
 
   users: {
     // Ditto as above.
