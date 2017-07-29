2e33c077df592fb797afeb47204fc0d41586a4a4
Hack Reactor Student <student@hackreactor.com>
2017-07-28 22:24:32 -0700

    Implement database GET

--- server/models/index.js
+++ server/models/index.js
@@ -2,11 +2,37 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.connection.query('SELECT messages.text, users.username FROM messages, users', function(error, results, field) {
+        if (error) { throw error; }
+        callback(results); 
+      });
+    },
+    post: function (body, callback) {
+      // db.connection.query(`insert into users (username) values('${body.username}')`, function(error, results, field) {
+      //   if (error) { throw error; }
+      // });
+      db.connection.query(`insert into messages (text) values('${body.text}')`, function(error, results, field) {
+        if (error) { throw error; }
+        callback();
+      });
+            
+      // db.connection.query(`insert into messages 
+      //   (text, user_id, room_id) 
+      //   values('${body.text}', select id from users where username='${body.username}', select id from rooms where roomname='${body.roomname}')`, 
+      // function(error, results, field) {
+      //   if (error) { throw error; }
+      // });
+    }
   },
 
   users: {
+    // Ditto as above.
+    get: function () {},
+    post: function () {}
+  }, 
+  
+  rooms: {
     // Ditto as above.
     get: function () {},
     post: function () {}
