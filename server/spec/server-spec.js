b11997513006961314135e9265182ae273aa4016
Hack Reactor Student <student@hackreactor.com>
2017-07-29 17:54:25 -0700

    Refactor to match implementation

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -10,8 +10,8 @@ describe('Persistent Node Chat Server', function() {
 
   beforeEach(function(done) {
     dbConnection = mysql.createConnection({
-      user: 'root',
-      password: '',
+      user: 'student',
+      password: 'student',
       database: 'chat'
     });
     dbConnection.connect();
@@ -30,41 +30,36 @@ describe('Persistent Node Chat Server', function() {
   });
 
   it('Should insert posted messages to the DB', function(done) {
-    // Post the user to the chat server.
+    // Post a message to the node chat server:
     request({
       method: 'POST',
-      uri: 'http://127.0.0.1:3000/classes/users',
-      json: { username: 'Valjean' }
+      uri: 'http://127.0.0.1:3000/classes/messages',
+      json: {
+        username: 'Valjean',
+        message: 'In mercy\'s name, three days is all I need.',
+        roomname: 'Hello'
+      }
     }, function () {
-      // Post a message to the node chat server:
-      request({
-        method: 'POST',
-        uri: 'http://127.0.0.1:3000/classes/messages',
-        json: {
-          username: 'Valjean',
-          message: 'In mercy\'s name, three days is all I need.',
-          roomname: 'Hello'
-        }
-      }, function () {
-        // Now if we look in the database, we should find the
-        // posted message there.
+      // Now if we look in the database, we should find the
+      // posted message there.
 
-        // TODO: You might have to change this test to get all the data from
-        // your message table, since this is schema-dependent.
-        var queryString = 'SELECT * FROM messages';
-        var queryArgs = [];
+      // TODO: You might have to change this test to get all the data from
+      // your message table, since this is schema-dependent.
+      var queryString = 'SELECT * FROM messages';
+      var queryArgs = [];
 
-        dbConnection.query(queryString, queryArgs, function(err, results) {
-          // Should have one result:
-          expect(results.length).to.equal(1);
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        console.log('RESULTS', results);
+        // Should have one result:
+        expect(results.length).to.equal(1);
 
-          // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
+        // TODO: If you don't have a column named text, change this test.
+        expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
 
-          done();
-        });
+        done();
       });
     });
+  
   });
 
   it('Should output all messages from the DB', function(done) {
